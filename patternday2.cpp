6198353d9bd73d9ae495759655e6be094a1a6b84
RAJ CHAUHAN <90085709+itsdevrajchauhan@users.noreply.github.com>
2025-09-09 00:44:03 +0530

    various of Patterns Printng

--- patternday2.cpp
+++ patternday2.cpp
@@ -20,7 +20,7 @@ int main()
     }
 
 
-    */
+
 
     int n = 10;
 
@@ -31,14 +31,134 @@ int main()
         {
             cout << '*';
         }
+
         cout << endl;
     }
 
-    return 0;
-}
 
-/* This will print this
-A B C
-D E F
-G E H
+
+
+    int n = 5;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+            cout << i;
+        cout << endl;
+    }
+
+
+    int n = 5;
+    char ch = 'A';
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+            cout << ch;
+        ch = ch + 1;
+        cout << endl;
+    }
+
+
+
+    int n = 5;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+
+            cout << j + 1;
+
+        cout << endl;
+    }
+
+
+
+
+
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j > 0; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+
+
+
+    int n = 5;
+    int num = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+
+
+
+
+    int n = 5;
+    char ch = 'A';
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+
+
+
+
+
+    int n = 5;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << (i+1);
+        }
+        cout << endl;
+    }
+
+
+
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        // spaces : n-i-1
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        // nums1 : i+1
+        for (int j = 1; j <= i + 1; j++)
+        {
+            cout << j;
+        }
+        // nums2 : reverse loop
+        for (int j = i; j > 0; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+};
+
+
+
+
 */
+
+ 
+};
