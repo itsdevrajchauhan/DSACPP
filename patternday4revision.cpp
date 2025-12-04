f525ec8f96754f390d5ceb18d77a5230a967aeb6
itsdevrajchauhan <rajchauhan6279@gmail.com>
2025-12-04 12:24:54 +0530

    Back to revison

--- patternday4revision.cpp
+++ patternday4revision.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int n;
+/*
+ int n;
     cout << "enter the number n :  " << endl;
     cin >> n;
 
-    for (int i = 1; i <=0n; i++)
+    for (int i = 1; i <=__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4; i++)
     {
         for (int j = 1; j <=n; j++)
         {
@@ -15,6 +14,247 @@ int main()
         }
         cout << endl;
     }
+
+    return 0;
+
+
+
+
+
+
+
+    ------------------------------------------------------------------------
+   star pattern
+
+
+    int n;
+    cout << "enter the number of  star's  : " << endl;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++ )
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    -----------------------------------------
+half pyramid
+
+   int n;
+    cout<<"enter the height of the pyramid  : " <<endl;
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+
+        for(int j=0;j<i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+
+===================================================
+
+ int n;
+ cout<<"abcd printning : "<<endl;
+ cin>>n;
+ for(int i=0;i<n;i++){
+    char ch ='A';
+    for(int j=0;j6
+        <n;j++){
+
+        cout<<ch;
+        ch=ch+1;
+
+    }
+    cout<<endl;
+ }
+
+
+return 0;5
+
+
+
+==================================================================
+ int n;
+ cout<<"enter the Number you wanted to print: "<<endl;
+ cin>>n;
+
+char ch='A';
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        cout<<ch;
+        ch++;
+    }
+    cout<<endl;
+}
+
+
+===================================================
+ int n;
+    cout << "enter the number " << endl;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+
+
+
+
+
+
+
+
+
+=======================================================================
+=  int n;
+    cout << "Enter the number you want  " << endl;
+    cin >> n;
+
+    for (int i = 0; i <n; i++)
+    {
+
+        for (int j = i+1; j >0; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+
+
+
+
+
+
+
+
+
+
+==================================================
+    int num = 1;
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << num;
+            num++;
+        }
+        cout << endl;
+    }
+
+    =========================================================
+
+      char ch = 'A';
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i+1; j++)
+        {
+            cout << ch<<" ";
+            ch++;
+        }
+        cout << endl;
+    }
+
+
+    =======================================================
+
+    for (int i = 0; i < n; i++)
+    {
+
+char ch = 'A'+i;
+        for (int j =i+1; j>0; j--)
+        {
+            cout << ch;
+            ch--;
+        }
+        cout << endl;
+    }
+
+    ----------------------------------------------------------------
+
+     int n;
+    cout << "enter the num : " << endl;
+    cin >> n;
+for(int i=0;i<n;i++){
+    for(int j=0;j<i+1;j++){
+        cout<<" ";
+    }
+    for(int j=1;j<n-i;j++){
+        cout<<i+1;
+    }
+    cout<<endl;
+}
+
+==============================================================
+ int n;
+    cout << "enter the num : " << endl;
+    cin >> n;
+for(int i=0;i<n;i++){
+    for(int j=0;j<i+1;j++){
+        cout<<" ";
+    }
+    for(int j=1;j<n-i;j++){
+        cout<<i+1;
+    }
+    cout<<endl;
+}
+-----------------------------------------------
+ char ch='A';
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            cout<<" ";
+
+        }
+        for(int j=0;j<n-i;j++){
+            cout<<ch;
+
+        }
+        ch++;
+        cout<<endl;
+    }
+
+    =====================================================
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j =i; j<n-1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i + 1; j++)
+        {
+            cout << j;
+        }
+          for (int j = i; j>0; j--)
+        {
+            cout << j;
+        }
+
+        cout << endl;
+    }
+
+*/
+
+int main()
+{
+    int n;
+    cout << "enter the num : " << endl;
+    cin >> n;
+
+
+
 
     return 0;
 }
